05c246cddae592f8653d605fc85e31fa8a53f0d0
Pedro Gomes <pedrogomes94@gmail.com>
2021-02-12 13:40:25 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'walletconnect-v2-react-demos'
+import 'walletconnect-v2-react-demos/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
